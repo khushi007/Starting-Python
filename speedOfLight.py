55ae05f49e44eaee8da13e20513cab542d790087
khushi007 <khushikaranpuria007@gmail.com>
2016-08-14 19:17:41 +0530

    Create speedOfLight

---
+++ speedOfLight.py
@@ -0,0 +1,8 @@
+speed_of_light = 299792458; #meteres per second
+speed_of_light_in_cms = speed_of_light*100;
+time = 1.0/1000000000; #one nanosecond
+dist = speed_of_light_in_cms * time;
+print dist;
+
+
+ 
